dbe784cd33ede188c554f17b89f70312bcf08ecc
chrisxwave <chrisilmer914@gmail.com>
2026-05-07 22:12:46 +0200

    terzo compito

--- script.js
+++ script.js
@@ -52,3 +52,32 @@ console.log(sum); // Output: 56
 //6.
 const numBytes = (numOne + numTwo + numThree) / numOne;
 console.log(numBytes); // Output: 7
+
+
+//Terzo esercizio: Operatori sulle stringhe
+
+const city = 'New York'; // NON MODIFICARE QUESTA RIGA
+const cityName = 'City'; // NON MODIFICARE QUESTA RIGA
+const shoppingList = 'mele, banane, arance, uva'; // NON MODIFICARE QUESTA RIGA
+const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
+
+
+//1.
+const cityIndex = city.indexOf('Y');
+console.log(cityIndex); // Output: 4
+
+//2.
+const citySubstring = city.substring(4);
+console.log(citySubstring); // Output: "York"
+
+//3.
+const cityReplaced = city.replace('York', 'Delhi');
+console.log(cityReplaced); // Output:  "New Delhi"
+
+//4.
+const cityNameConcat = `${city} ${cityName}`;
+console.log(cityNameConcat); // Output: "New York City"
+
+//5.
+const upperCaseHello = hello.toUpperCase();
+console.log(upperCaseHello); // Output: "HELLO"
