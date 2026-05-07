2c057c9291b556bc1546d956a9633943b0352022
chrisxwave <chrisilmer914@gmail.com>
2026-05-07 21:45:54 +0200

    commit primo compito

---
+++ script.js
@@ -0,0 +1,24 @@
+//Dichiarazione variabile
+let name= 'Chris';
+console.log(name);
+
+//Tipi primitivi
+let number = 42;
+let text = 'Hello, World!';
+let bool = true;
+console.log(number);
+console.log(text);
+console.log(bool);
+
+//Sostituisci null con operazioni con le stringhe
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//Imposta questa variabile utilizzando una proprietà sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = alphabet.length;
+console.log(numberOfLettersInAlphabet);
